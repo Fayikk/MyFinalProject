68a3914ceb0c0b8dc6a6227af9c1d8c632aab74d
Fayik <veznedaroglufayik2@gmail.com>
2022-09-10 20:47:22 +0300

    Update

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -10,7 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Business.Concrete
-{
+{   //Trying
     public class ProductManager : IProductService
     {
         IProductDal _productDal;
