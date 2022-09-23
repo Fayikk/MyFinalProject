584aeb78fb361d11bce90c8619fbb88534466af1
Fayik <veznedaroglufayik2@gmail.com>
2022-09-23 09:47:04 +0300

    Updated

---
+++ Core/DependencyResolvers/CoreModule.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DependencyResolvers
+{
+    //Bu class bizim uygulama seviyesinde bağımlılıklarımızı çözeceğimiz yer olacaktır.
+    public class CoreModule : ICoreModule
+    {
+        public void Load(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        }
+    }
+}
---
+++ Core/Utilities/IoC/ICoreModule.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.IoC
+{
+    //Framework katmanımımız
+    public interface ICoreModule
+    {
+        void Load(IServiceCollection serviceCollection);
+    }
+}
